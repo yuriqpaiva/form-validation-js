e5da6212654da55a503ecbc20b144e9a61c69317
Yuri <yuriqpaiva@gmail.com>
2022-01-22 18:17:12 -0300

    money mask on price input]

--- js/validacao.js
+++ js/validacao.js
@@ -58,6 +58,9 @@ const mensagensDeErro = {
   estado: {
     valueMissing: 'O campo de estado não pode estar vazio',
   },
+  preco: {
+    valueMissing: 'O campo de preço não pode estar vazio',
+  },
 };
 
 const validadores = {
